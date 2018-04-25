b5b41451e680dbc01d0ea00b2231f6f37c6c5e33
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2018-04-25 21:34:52 +0700

    kocnhenie slideri

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,27 @@ const tabsClick = function(){
     }
   })
 }()
+
+const flotSlider = function(slider,thumb){
+  var galleryTop = new Swiper(slider, {
+    slidesPerView: 1,
+    initialSlide:1,
+    navigation: {
+      nextEl: '.Flot__slider_button_next',
+      prevEl: '.Flot__slider_button_prev',
+    },
+  });
+  var galleryThumbs = new Swiper(thumb, {
+      spaceBetween: 10,
+      centeredSlides: true,
+      initialSlide:1,
+      slidesPerView: 3,
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+}
+flotSlider('#Flot-first-top','#Flot-first-thumb')
+// flotSlider('#Flot-second-top','#Flot-second-thumb')
+// flotSlider('#Flot-third-top','#Flot-third-thumb')
